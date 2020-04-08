a65846ac36d6dfe86b76445d589e978ec8116296
Kirill Sidorov <kirillsidorov@gmail.com>
2020-04-08 20:09:11 +0300

    Error popup

---
+++ assets/js/ui/Popups/ErrorFeedBackPopup.js
@@ -0,0 +1,40 @@
+import Popup from './Popup';
+
+class ErrorFeedBackPopup extends Popup{
+
+  _message= '';
+
+  constructor() {
+    super();
+  }
+
+  set message(value) {
+    this._message = value;
+  }
+
+  get message() {
+    return this._message || '';
+  }
+
+
+  renderPopupBody() {
+    return `<h4 class="popup__title">Возникала ошибка!</h4>
+            <div class="popup-order">
+              <div class="popup-order__lead">
+                 ${this.message}
+              </div>
+              <div class="popup-order__lead">
+                 В ближайшее время с вами свяжется наш менеджер <br> для уточнения заказа
+              </div>
+              <img class="popup-order__img" src="/img/face_medved.png" alt="генеральный директор АвтоМагист">
+              <div class="popup-order__note">
+                  «Мы очень внимательно относимся к пожеланиям клиентов,
+                  поэтому если ошибка будет повторяться, <br>
+                  огромная просьба – сообщите нам —
+                 <a href="mailto:info@automagistre.ru">info@automagistre.ru</a>»
+              </div>
+            </div>`;
+  }
+}
+
+export default ErrorFeedBackPopup;
