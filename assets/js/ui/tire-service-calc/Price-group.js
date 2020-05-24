3079e86bd1940103a1f30a4eaa76ec0e083600d4
Kirill Sidorov <kirillsidorov@gmail.com>
2020-05-24 18:22:33 +0300

    tire service calculator

---
+++ assets/js/ui/tire-service-calc/Price-group.js
@@ -0,0 +1,46 @@
+import PriceItem from './Price-item';
+
+class PriceGroup {
+
+  _items = []
+
+  constructor(group) {
+    this._name = group.name
+    this._node = this._createNode()
+    if (group.services) {
+      for (const service of group.services) {
+        this.addPriceItem(new PriceItem(service));
+      }
+    }
+  }
+
+  get name() {
+    return this._name
+  }
+
+  addPriceItem(priceItem) {
+    this._items.push(priceItem)
+  }
+
+  _createNode() {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = `<div class="prices_group">
+                            <h2 class="sec-about__title">${this.name}</h2> 
+                            <div class="prices sec-about__prices"></div>
+                         </div>`
+    return wrapper.firstElementChild
+  }
+
+  updatePrice(tireSelector, carSelector) {
+    this._items.forEach(item => item.updatePrice(tireSelector, carSelector))
+  }
+
+  render(node) {
+    const itemsNode = this._node.querySelector('.prices.sec-about__prices')
+    this._items.forEach(item => item.render(itemsNode))
+    node.append(this._node)
+  }
+
+}
+
+export default PriceGroup;
---
+++ assets/js/ui/tire-service-calc/Price-item.js
@@ -0,0 +1,57 @@
+import PriceCalculatorFactory from './Price-calculator';
+
+class PriceItem {
+
+  _name = undefined
+  _price = undefined
+
+  constructor(item) {
+    this._name = item.name
+    this._id = item.id
+    this._calculator = PriceCalculatorFactory.getPriceCalculator(this._id)
+    this._node = this._createNode()
+    this._inputNode = this._node.querySelector('input')
+  }
+
+  _createNode () {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = `<div class="prices__row">
+                            <div class="prices__check">
+                                <label class="form__checkbox">
+                                    <input type="checkbox">
+                                    <span></span>
+                                </label>
+                            </div>
+                            <div class="prices__note">${this.name}</div>
+                            <div class="prices__cost">${this.price}<i class="icon-rub">a</i></div>
+                         </div>`
+    return wrapper.firstElementChild
+  }
+
+  get name() {
+    return this._name
+  }
+
+  get price() {
+    return this._price
+  }
+
+  get isChecked () {
+    return this._inputNode.checked
+  }
+
+  set price(value) {
+    this._price = +value
+    this._node.querySelector('.prices__cost').innerHTML = `${this.price}<i class="icon-rub">a</i>`
+  }
+
+  updatePrice(tireSelector, carSelector) {
+    this.price = this._calculator(tireSelector, carSelector)
+  }
+
+  render(node) {
+    node.append(this._node)
+  }
+}
+
+export default PriceItem
