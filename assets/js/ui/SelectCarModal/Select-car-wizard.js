b703287f7e6b9b77c965a92b89d0aa2232ea2ce9
Kirill Sidorov <kirillsidorov@gmail.com>
2020-06-03 19:21:54 +0300

    Slick slider on modal

---
+++ assets/js/ui/SelectCarModal/Select-car-wizard.js
@@ -0,0 +1,41 @@
+import $ from 'jquery'
+import 'slick-carousel'
+
+class SelectCarWizard {
+
+  steps = []
+
+  constructor(node) {
+    this.steps[0] = new  SelectCarWizardStep1()
+  }
+
+}
+
+
+
+class SelectCarWizardStep1 {
+
+  constructor() {
+    const options = {
+      arrows: true,
+      dots: false,
+      infinite: true,
+      speed: 0,
+      fade: true,
+      autoplay: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type=\'button\' class=\'slick-arrow slick-prev\'></button>',
+      nextArrow: '<button type=\'button\' class=\'slick-arrow slick-next\'></button>',
+      responsive: [
+        {breakpoint: 768, settings: {arrows: false}},
+      ],
+    }
+
+    this.slider = $('#select-car-slider').slick(options)
+      
+  }
+}
+
+
+export default SelectCarWizard
