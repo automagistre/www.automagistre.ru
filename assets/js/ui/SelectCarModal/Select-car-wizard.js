ea404e3ec9aa865d844c20b7c7c9784f1815e79d
Kirill Sidorov <kirillsidorov@gmail.com>
2020-06-03 23:46:00 +0300

    Step manufacturer

--- assets/js/ui/SelectCarModal/Select-car-wizard.js
+++ assets/js/ui/SelectCarModal/Select-car-wizard.js
@@ -6,16 +6,52 @@ class SelectCarWizard {
   steps = []
 
   constructor(node) {
-    this.steps[0] = new  SelectCarWizardStep1()
+    this.steps[0] = new  SelectCarWizardStepManufacturer(node)
+    this.steps[0].setActive(this.steps[0])
+    node.querySelectorAll('.js-select-manufacturer').forEach( node=> {
+      node.addEventListener('click', ()=> this.changeStep(1))
+    })
   }
 
+  changeStep(num) {
+    for (let i = 0; i < this.steps.length; i++) {
+      this.steps[i].setComplete(i < num)
+      this.steps[i].setActive(this.steps[num])
+    }
+  }
 }
 
 
+class SelectCarWizardStep {
 
-class SelectCarWizardStep1 {
+  _selectedContent = {}
+  _indicatorNode = undefined
+  _node = undefined
 
-  constructor() {
+  setActive(entity) {
+    this._indicatorNode.classList.toggle('is-active', entity === this)
+    this._node.classList.toggle('is-active', entity === this)
+    if (this.isActive) this._indicatorNode.classList.remove('is-complete')
+  }
+
+  setComplete(test) {
+    this._indicatorNode.classList.toggle('is-complete', test)
+  }
+
+  get content() {
+    return this._selectedContent
+  }
+
+  get isActive() {
+    return this._indicatorNode.classList.contains('is-active')
+  }
+}
+
+
+class SelectCarWizardStepManufacturer extends SelectCarWizardStep {
+
+  constructor(node) {
+    super()
     const options = {
       arrows: true,
       dots: false,
@@ -36,12 +72,16 @@ class SelectCarWizardStep1 {
 
     document.querySelectorAll('.js-set-start-slide').forEach(btnNode => {
       const sliderToGo = btnNode.dataset.slide || 0;
+      const manufacturer = btnNode.dataset.content;
       btnNode.addEventListener('click', ()=> {
         this.$slider.slick('slickGoTo', sliderToGo, false)
+        this._selectedContent.manufacturer = manufacturer
       })
     })
-
+    this._indicatorNode = node.querySelector('.modal__step[data-step="Manufacturer"]')
+    this._node = node.querySelector('#modal-tab_01')
   }
+
 }
 
 
