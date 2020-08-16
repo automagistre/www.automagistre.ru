7e0726d2b8c740acfb852bdcd062ecc2677e0454
Kirill Sidorov <kirillsidorov@gmail.com>
2020-08-16 12:35:27 +0300

    mongo schema

---
+++ apollo_server/mongo/maintenanceShema.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const MaintenanceSchema = new Schema({
+  _id: String,
+  engine: {
+    airIntake: String,
+    capacity: String,
+    injection: String,
+    name: String,
+    type: String,
+  },
+  transmission: String,
+  vehicle: {
+    _id: String,
+    caseName: String,
+    localizedName: String,
+    manufacturer: {
+      id: String,
+      localizedName: String,
+      name: String,
+    },
+    name: String,
+    yearFrom: String,
+    yearTill: String,
+  },
+  wheelDrive: String,
+  works: [{
+    description: String,
+    name: String,
+    parts: [
+      {
+        part: {
+          discount: {
+            amount: String,
+            currency: String,
+          },
+          id: String,
+          manufacturer: {
+            id: String,
+            localizedName: String,
+            name: String,
+          },
+          name: String,
+          number: String,
+          price: {
+            amount: String,
+            currency: String,
+          },
+          universal: Boolean
+        },
+        quantity: Number,
+        recommended: Boolean
+      }
+    ],
+    period: Number,
+    price: [
+      {
+        amount: String,
+        currency: String,
+      }
+    ],
+    recommended: Boolean
+  }]
+})
+
+module.exports = mongoose.model('maintenance', MaintenanceSchema, 'maintenance')
