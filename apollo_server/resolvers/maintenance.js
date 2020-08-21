c32884637bd5583fa0c3b3cc02e8b331ceebba13
Kirill Sidorov <kirillsidorov@gmail.com>
2020-08-21 15:35:14 +0300

    API resolvers

--- apollo_server/resolvers/maintenance.js
+++ apollo_server/resolvers/maintenance.js
@@ -2,14 +2,23 @@ import Maintenance from '../mongo/maintenanceShema';
 
 const  resolvers = {
   Query: {
-    async maintenances() {
-      return await Maintenance.find()
+
+    async getVehiclesByManufacturer(_, args) {
+      return Maintenance.find({'vehicle.manufacturer.name': args.manufacturer}, {vehicle: 1, _id: 0}).distinct('vehicle')
     },
-    async maintenanceByCaseName(parent, args) {
-      return Maintenance.findOne({
-        'vehicle.caseName': args.caseName,
-        'vehicle.manufacturer.name': args.manufacturer
-      }).exec()
+
+    async getVehicleByID(_, args) {
+      const vehicle = await Maintenance.findOne({'vehicle._id': args.id}, {vehicle: 1})
+      return vehicle.vehicle
+    },
+
+    async getWorksByMaintenanceID(_, args) {
+      const maintenance = await Maintenance.findById(args.id)
+      return maintenance.works
+    },
+
+    async maintenancesByVehicleID(_, args) {
+      return  Maintenance.find({'vehicle._id': args.id}).exec()
     }
   }
 }
