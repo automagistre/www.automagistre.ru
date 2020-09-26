8f6105fb06e2dfca46438043d88f7fcca32c9203
Kirill Sidorov <kirillsidorov@gmail.com>
2020-09-26 23:34:29 +0300

    show popup in dev links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import './hoc/service'
 import {mobChecker} from "./lib";
 import ServerData from './helpers/ServerData';
+import inDevPopup from './ui/Popups/InDevPopup';
 
 const BODY = document.body;
 
@@ -11,3 +12,13 @@ isMobileView = mobChecker(1024);
 setTimeout(function() {
     BODY.classList.remove('no-start-animations');
 }, 500);
+
+
+document.querySelectorAll('.js-in-dev').forEach(link => {
+    link.addEventListener('click', e =>{
+        e.preventDefault()
+        const popup = new inDevPopup()
+        popup.message = 'Мы работаем над новым функционалом, в скором времени все заработает'
+        popup.open()
+    })
+})
