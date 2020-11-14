e923895313c40f77984cc300269d27a58f15239a
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-14 18:59:43 +0300

    added wrapper for spinner for center view

--- assets/js/ui/LoadingSpinner.js
+++ assets/js/ui/LoadingSpinner.js
@@ -8,11 +8,13 @@ class LoadingSpinner {
   show() {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = `
-                    <div class="spinner is-loading " style="transform: scale(${this._size})">
-                        <div class="spinner__dot spinner__dot_01"></div><div class="spinner__dot spinner__dot_02"></div>
-                        <div class="spinner__dot spinner__dot_03"></div><div class="spinner__dot spinner__dot_04"></div>
-                        <div class="spinner__dot spinner__dot_05"></div><div class="spinner__dot spinner__dot_06"></div>
-                        <div class="spinner__dot spinner__dot_07"></div><div class="spinner__dot spinner__dot_08"></div>
+                    <div class="spinner__wrapper">
+                      <div class="spinner is-loading " style="transform: scale(${this._size})">
+                          <div class="spinner__dot spinner__dot_01"></div><div class="spinner__dot spinner__dot_02"></div>
+                          <div class="spinner__dot spinner__dot_03"></div><div class="spinner__dot spinner__dot_04"></div>
+                          <div class="spinner__dot spinner__dot_05"></div><div class="spinner__dot spinner__dot_06"></div>
+                          <div class="spinner__dot spinner__dot_07"></div><div class="spinner__dot spinner__dot_08"></div>
+                      </div>
                     </div>`
     this._spinnerNode = wrapper.firstElementChild
     this._node.appendChild(this._spinnerNode)
