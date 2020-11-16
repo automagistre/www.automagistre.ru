1f2ba6aa2e22772e39f8758dbd6c5a33c3afce0d
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-16 17:27:03 +0300

    replace malihu-scrollbar to perfect-scrollbar

--- assets/js/hoc/service.js
+++ assets/js/hoc/service.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-import 'malihu-custom-scrollbar-plugin';
+import PerfectScrollbar from 'perfect-scrollbar'
 import 'jquery-scrollify'
 import {mobChecker} from "../lib";
 import '../sections/start'
@@ -19,7 +19,7 @@ if (document.location.pathname.split('/')[2]) {
     localStorageManager.manufacturer = manufacturer
 }
 
-const header = Header.instance;
+Header.instance;
 
 new ScrollToTop();
 
@@ -38,43 +38,22 @@ goToButtons.forEach(btn => {
         btn.addEventListener('click', ()=>animateScrollTo(scrollTarget))
 });
 
-const $scroll_X = $('.js-scroll-x'),
-      $scroll_Y = $('.js-scroll-y');
+const customScrollBarOptions = {
+    handlers: ['click-rail', 'drag-thumb', 'wheel', 'touch'],
+    swipeEasing: true,
+    maxScrollbarLength: 150
+}
 
-export const initScroll_X = $elem => {
-    $elem.mCustomScrollbar({
-        axis: 'x',
-        scrollButtons: {enable: false},
-        scrollbarPosition: 'outside',
-        alwaysShowScrollbar: 2,
-        updateOnContentResize: true,
-        autoDraggerLength: false,
-        mouseWheel: {enable: false},
-    });
-};
-
-export const initScroll_Y = $elem => {
-    $elem.mCustomScrollbar({
-        axis: 'y',
-        scrollButtons: {enable: false},
-        scrollbarPosition: 'outside',
-        alwaysShowScrollbar: 0,
-        autoDraggerLength: false,
-        advanced: {
-            updateOnContentResize: true
-        }
-    });
-};
+document.querySelectorAll('.js-scroll-x').forEach(el => new PerfectScrollbar(el, {
+    ...customScrollBarOptions,
+    suppressScrollY: true
+}))
+document.querySelectorAll('.js-scroll-y').forEach(el => new PerfectScrollbar(el, {
+    ...customScrollBarOptions,
+    suppressScrollX: true
+}))
 
 $(window).on('load', ()=> {
-    if ($scroll_X.length) {
-        initScroll_X($scroll_X);
-        $(window).resize(() => {
-            $scroll_X.mCustomScrollbar('destroy');
-            initScroll_X($scroll_X);
-        });
-    }
-    if ($scroll_Y.length) initScroll_Y($scroll_Y);
     $(window).resize(() => mobChecker(1024) ? $.scrollify.disable() : $.scrollify.enable());
 });
 
