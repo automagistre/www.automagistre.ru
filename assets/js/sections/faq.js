03f949a30bc686f4c5b5b1748052678f7ea46ab7
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-16 17:48:47 +0300

    added scroll indicator in faq tabs

--- assets/js/sections/faq.js
+++ assets/js/sections/faq.js
@@ -1,11 +1,18 @@
 import {initTabs} from "../lib";
 import {SubscribeForm} from '../ui/forms';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 const faqSec = () => {
   const $form  = document.querySelector('section.sec-faq')
                          .querySelector('.sec-faq__form');
   const tabs = 'sec-faq-tabs',
         body = 'sec-faq-body';
+  new PerfectScrollbar('#sec-faq-tabs', {
+    handlers: ['touch'],
+    swipeEasing: true,
+    maxScrollbarLength: 150,
+    suppressScrollY: true
+  })
   initTabs(tabs, body);
   new SubscribeForm($form);
 };
