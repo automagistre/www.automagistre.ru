bab3c8e27b72e88161848867aac92cea15892d6d
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-18 13:29:37 +0300

    added intro section

---
+++ assets/js/sections/intro.js
@@ -0,0 +1,25 @@
+import {SubscribeForm} from '../ui/forms';
+import ServerDataSender from '../helpers/server-data-sender';
+import SuccessFeedBackPopup from '../ui/Popups/SuccessFeedBackPopup';
+import ErrorFeedBackPopup from '../ui/Popups/ErrorFeedBackPopup';
+
+const introSec = () => {
+  const formNode  = document.querySelector('section.sec-intro')
+  .querySelector('.sec-intro__form');
+
+  if (!formNode) return
+
+  const scheduleForm = new SubscribeForm(formNode, 'schedule');
+  const dataSender = new ServerDataSender()
+  dataSender.onSuccess = () => {
+    (new SuccessFeedBackPopup('Мы уже обрабатываем вашу заявку.')).open()
+  }
+  dataSender.onError = () => {
+    (new ErrorFeedBackPopup('Ошибка соединения, повторите попытку')).open()
+  }
+  formNode.querySelector('a[data-type="submit"]').addEventListener('click', async ()=>{
+    if (scheduleForm.isValid) await dataSender.sendForm(scheduleForm)
+  })
+}
+
+export default introSec
