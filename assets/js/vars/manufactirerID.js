012adfdcd337a28618d1e55a4e7d7ab4bddd4662
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-26 22:59:19 +0300

    get vehicals by manufactirerID

--- assets/js/vars/manufactirerID.js
+++ assets/js/vars/manufactirerID.js
@@ -1,4 +1,4 @@
-export const manufactirerID = {
+export const manufacturerID = {
   'toyota': '1ea880a0-beb1-6c00-93f4-02420a0002b6',
   'nissan': '1ea880a0-beb1-628c-ad31-02420a0002b6',
   'infiniti': '1ea880a0-beca-6a48-9341-02420a0002b6',
