8f19a0a34b863bd27729e2b6fd770ca2a5e2440c
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-27 16:56:06 +0300

    added work position to server

--- apollo_server/mongo/maintenanceShema.js
+++ apollo_server/mongo/maintenanceShema.js
@@ -61,7 +61,8 @@ const MaintenanceSchema = new Schema({
         amount: String,
         currency: String,
     },
-    recommended: Boolean
+    recommended: Boolean,
+    position: Number
   }]
 })
 
