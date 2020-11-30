8102869b043e15fc1fbee6b09d2f62063faf9825
Kirill Sidorov <kirillsidorov@gmail.com>
2020-11-30 16:35:32 +0300

    chunk experience, facts css

--- assets/js/sections/experience.js
+++ assets/js/sections/experience.js
@@ -1,7 +1,9 @@
 import {odometer} from '../lib';
+import '../../less/4_sections/sec_experience.less'
+import '../../less/3_blocks/block_facts.less'
 
 const experienceSec = () => {
-  odometer('js-odometer')
+  import('../../less/2_plugins/odometer.less').then(() => odometer('js-odometer'))
 };
 
 export default experienceSec;
