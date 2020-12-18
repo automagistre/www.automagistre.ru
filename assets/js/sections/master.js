07d7c78d73e4a52e907c37379fdc2b304316f817
Kirill Sidorov <kirillsidorov@gmail.com>
2020-12-18 23:46:54 +0300

    final master.html.twig swiper

--- assets/js/sections/master.js
+++ assets/js/sections/master.js
@@ -1,26 +1,47 @@
 import Swiper, {Pagination, Navigation} from 'swiper';
-import '../../less/2_plugins/swiper'
+import '../../less/2_plugins/swiper/swiper'
+import '../../less/2_plugins/swiper/pagination'
+
 
 const secMaster = () => {
+
   const startSlide = Math.floor(Math.random() * Math.floor(5))
   Swiper.use([Navigation, Pagination])
-  const swiper = new Swiper(document.getElementById('sec-master-slider'), {
+
+  const checkArrow = swiperNode => {
+    const swiperPrev = swiperNode.querySelector('.slick-prev'),
+          swiperNext = swiperNode.querySelector('.slick-next')
+
+    swiperNext.classList.toggle('is-hidden', window.innerWidth <= 760)
+    swiperPrev.classList.toggle('is-hidden', window.innerWidth <= 760)
+
+  }
+  const swiperNode = document.getElementById('sec-master-slider')
+  new Swiper(swiperNode, {
     speed: 800,
     loop: true,
     initialSlide: startSlide,
     navigation: {
       nextEl: '.slick-next',
       prevEl: '.slick-prev',
+      hiddenClass: 'is-hidden'
     },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    on:{
+      init: () => checkArrow(swiperNode),
+      resize: () => checkArrow(swiperNode)
+    }
   })
 }
 
 export default secMaster;
 
 //
-// arrows: true,
+
 //     dots: true,
-//     responsive: [
-//   {breakpoint: 760, settings: {arrows: false}},
-// ],
+
 // }
