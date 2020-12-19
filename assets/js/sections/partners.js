02a15acda783d08e4dfa62235f734156071590db
Kirill Sidorov <kirillsidorov@gmail.com>
2020-12-19 15:08:59 +0300

    swiper configure

--- assets/js/sections/partners.js
+++ assets/js/sections/partners.js
@@ -1,28 +1,50 @@
-import $ from 'jquery';
-
-
-const initSlickSlider = () => {
-  const slickOptions = {
-    arrows: true,
-    dots: true,
-    infinite: true,
-    speed: 800,
-    autoplay: true,
-    slidesToScroll: 1,
-    variableWidth: true,
-    prevArrow: '<button type=\'button\' class=\'slick-arrow slick-prev\'></button>',
-    nextArrow: '<button type=\'button\' class=\'slick-arrow slick-next\'></button>',
-    responsive: [
-      {breakpoint: 1024, settings: {arrows: false}},
-    ],
-  };
-
-  const $secPartnersSlider = $('#sec-partners-slider, .sec-partners-slider');
-  if ($secPartnersSlider.length) $secPartnersSlider.slick(slickOptions);
-};
+import Swiper, {Pagination, Navigation, Autoplay} from 'swiper';
+import '../../less/2_plugins/swiper/swiper'
+import '../../less/2_plugins/swiper/pagination'
+import '../../less/2_plugins/swiper/navigation'
+import {nodesObserver} from '../lib';
 
 const partnersSec = () => {
-  initSlickSlider()
+  Swiper.use([Navigation, Pagination, Autoplay])
+
+  const checkArrow = btns => {
+    for (let btn of btns)
+      btn.classList.toggle('is-hidden', window.innerWidth <= 1024)
+  }
+
+  const secNode = document.querySelector('section.sec-partners')
+  const swiperNode = document.getElementById('sec-partners-slider')
+
+  const prevBtn = secNode.querySelector('.slick-prev')
+  const nextBtn = secNode.querySelector('.slick-next')
+
+
+  const swiper = new Swiper(swiperNode, {
+    speed: 800,
+    loop: true,
+    slidesPerView: 'auto',
+    autoplay: {
+      delay: 2000,
+    },
+    navigation: {
+      nextEl: nextBtn,
+      prevEl: prevBtn,
+      hiddenClass: 'is-hidden'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    on:{
+      init: () => checkArrow([prevBtn, nextBtn]),
+      resize: () => checkArrow([prevBtn, nextBtn])
+    }
+  })
+  swiper.autoplay.stop()
+
+  nodesObserver([secNode], ()=> swiper.autoplay.start())
+
 };
 
 export default partnersSec;
