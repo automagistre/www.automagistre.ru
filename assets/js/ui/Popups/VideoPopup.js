0ea0dc9ae98be56c3aa15e62d39303e9e2251474
Kirill Sidorov <kirillsidorov@gmail.com>
2020-12-20 22:46:43 +0300

    Added video popup

---
+++ assets/js/ui/Popups/VideoPopup.js
@@ -0,0 +1,32 @@
+import Popup from './Popup';
+import YTplayer from 'yt-player'
+
+class VideoPopup extends Popup {
+  constructor(videoId) {
+    super();
+    this._videoId = videoId
+  }
+
+  renderPopupBody() {
+    return `<div class="popup__video">
+                <div class="popup__youtube js-popbox-youtube"></div>
+            </div>`
+  }
+
+  async onOpen() {
+    const opt = {
+      width: 640,
+      height: 360,
+      autoplay: true,
+
+    }
+    const wrapper = this._node.querySelector('.popup__wrap')
+    wrapper.classList.add('for_video')
+    this.player = new YTplayer(this._node.querySelector('.js-popbox-youtube'), opt)
+    await this.player.load(this._videoId)
+    this.player.play()
+
+  }
+}
+
+export default VideoPopup
