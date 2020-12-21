d7a1024be173a07d63a3bb13e203d829f2b75a90
Kirill Sidorov <kirillsidorov@gmail.com>
2020-12-22 00:10:58 +0300

    announces refactoring

--- assets/js/sections/announces.js
+++ assets/js/sections/announces.js
@@ -1,25 +1,38 @@
-import $ from 'jquery';
-import 'slick-carousel'
+import Swiper, {Pagination, Navigation} from 'swiper';
+import '../../less/2_plugins/swiper/swiper'
+import '../../less/2_plugins/swiper/pagination'
 import '../../less/4_sections/sec_announces'
 import '../../less/3_blocks/block_blog-card'
 
 const initSlickSlider = () => {
-  const slickOptions = {
-    arrows: true,
-    dots: true,
-    infinite: false,
-    speed: 800,
-    autoplay: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<button type=\'button\' class=\'slick-arrow slick-prev\'></button>',
-    nextArrow: '<button type=\'button\' class=\'slick-arrow slick-next\'></button>',
-    responsive: [
-      {breakpoint: 1024, settings: {slidesToShow: 2}},
-      {breakpoint: 600, settings: {slidesToShow: 1}},
-    ],
-  };
-  $('.js-announces-slider').slick(slickOptions)
+  Swiper.use([Pagination, Navigation])
+
+  document.querySelectorAll('.js-announces-slider').forEach(node => {
+    const nextArrow = node.querySelector('.slick-next'),
+          prevArrow = node.querySelector('.slick-prev')
+    new Swiper(node.firstElementChild, {
+      speed: 800,
+      navigation: {
+        nextEl: nextArrow,
+        prevEl: prevArrow,
+        disabledClass: 'is-disable'
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3
+        },
+        600: {
+          slidesPerView: 2
+        }
+      }
+
+    })
+  })
 };
 
 const secAnnounces = () => {
