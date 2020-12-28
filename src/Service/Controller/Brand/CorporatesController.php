b37f050db0e5be6be3a8171a2d5e3cf9cd26fd38
Konstantin Grachev <me@grachevko.ru>
2020-12-28 21:36:15 +0300

    Remove unused symfony/form and symfony/mailer

--- src/Service/Controller/Brand/CorporatesController.php
+++ src/Service/Controller/Brand/CorporatesController.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace App\Service\Controller\Brand;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -21,41 +17,9 @@ final class CorporatesController extends AbstractController
     /**
      * @Route("/corporates", name="corporates")
      */
-    public function __invoke(Request $request, MailerInterface $mailer): Response
+    public function __invoke(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('name')
-            ->add('telephone')
-            ->add('checkbox', CheckboxType::class)
-            ->getForm()
-            ->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = (object) $form->getData();
-
-            $message = (new Email())
-                ->from(new Address('no-reply@automagistre.ru', 'Автомагистр'))
-                ->to('info@automagistre.ru')
-                ->subject('Запись на корпоративное обслуживание')
-                ->text(<<<TEXT
-                    Имя: {$data->name}
-                    Телефон: {$data->telephone}
-                    TEXT
-                );
-
-            $mailer->send($message);
-
-            return new Response('OK');
-        }
-
-        if ($form->isSubmitted() && !$form->isValid()) {
-            return $this->json([
-                'error' => (string) $form->getErrors(),
-            ]);
-        }
-
         return $this->render('corporates.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
--- src/Service/Controller/Brand/FaqController.php
+++ src/Service/Controller/Brand/FaqController.php
@@ -5,13 +5,8 @@ declare(strict_types=1);
 namespace App\Service\Controller\Brand;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -22,45 +17,9 @@ final class FaqController extends AbstractController
     /**
      * @Route("/faq", name="faq", methods={"POST"})
      */
-    public function __invoke(Request $request, MailerInterface $mailer): Response
+    public function __invoke(Request $request): Response
     {
-        $form = $this->createFormBuilder(null, [
-            'action' => $this->generateUrl('faq'),
-        ])
-            ->add('name')
-            ->add('email', EmailType::class)
-            ->add('question', TextareaType::class)
-            ->getForm()
-            ->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = (object) $form->getData();
-
-            $message = (new Email())
-                ->from(new Address('no-reply@automagistre.ru', 'Автомагистр'))
-                ->replyTo($data->email)
-                ->to('info@automagistre.ru')
-                ->subject(\sprintf('FAQ Вопрос от %s', $data->name))
-                ->text(<<<TEXT
-                    Имя: {$data->name}
-                    Почта: {$data->email}
-                    Вопрос: {$data->question}
-                    TEXT
-                );
-
-            $mailer->send($message);
-
-            return new Response('OK');
-        }
-
-        if ($form->isSubmitted() && !$form->isValid()) {
-            return $this->json([
-                'error' => (string) $form->getErrors(),
-            ]);
-        }
-
         return $this->render('_sections/faq.html.twig', [
-            'form' => $form->createView(),
             'scroll' => $request->query->getBoolean('scroll'),
         ]);
     }
