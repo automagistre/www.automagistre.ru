09420341acc271fa6b7503967eff0e054b68bef4
Konstantin Grachev <me@grachevko.ru>
2020-12-28 21:05:51 +0300

    Sitemap: Add service section

---
+++ src/Service/Sitemap/SitemapListener.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Sitemap;
+
+use App\Service\Constants;
+use Presta\SitemapBundle\Event\SitemapPopulateEvent;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+final class SitemapListener implements EventSubscriberInterface
+{
+    private const ROUTES = [
+        'contacts',
+        'corporates',
+        'diagnostic_free',
+        'diagnostic_comp',
+        'faq',
+        'maintenance',
+        'reviews',
+        'service',
+        'tire',
+    ];
+
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SitemapPopulateEvent::ON_SITEMAP_POPULATE => 'onEvent',
+        ];
+    }
+
+    public function onEvent(SitemapPopulateEvent $event): void
+    {
+        $urlContainer = $event->getUrlContainer();
+
+        foreach (self::ROUTES as $route) {
+            foreach (Constants::BRANDS as $brand) {
+                $loc = $this->router->generate($route, [
+                    'brand' => $brand,
+                ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+                $urlContainer->addUrl(
+                    new UrlConcrete(
+                        $loc,
+                        null,
+                        UrlConcrete::CHANGEFREQ_WEEKLY,
+                        1
+                    ),
+                    'service',
+                );
+            }
+        }
+    }
+}
