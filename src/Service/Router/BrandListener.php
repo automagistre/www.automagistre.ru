c4406c32d88a3c426e60789b040b26ac70ca5d96
Konstantin Grachev <me@grachevko.ru>
2021-01-05 00:21:17 +0300

    Intercept Brand query parameter

--- src/Service/Router/BrandListener.php
+++ src/Service/Router/BrandListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service\Router;
 
+use App\Service\Constants;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -29,6 +30,16 @@ final class BrandListener implements EventSubscriberInterface
     {
         $request = $event->getRequest();
         $session = $request->getSession();
+
+        if (
+            $request->query->has('brand')
+            && \in_array($brand = $request->query->get('brand'), Constants::BRANDS, true)
+        ) {
+            $session->set(self::BRAND_SESSION, $brand);
+            $request->attributes->set('brand', $brand);
+
+            return;
+        }
 
         $brand = $request->attributes->get('brand');
         $brandSession = $session->get(self::BRAND_SESSION);
