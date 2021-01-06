c46d283f4e06523353114816d7c0aa76f6b07267
Kirill Sidorov <kirillsidorov@gmail.com>
2021-01-06 18:53:30 +0300

    Kia, hyundai in vars

--- assets/js/vars/manufactirerID.js
+++ assets/js/vars/manufactirerID.js
@@ -2,5 +2,7 @@ export const manufacturerID = {
   'toyota': '1ea880a0-beb1-6c00-93f4-02420a0002b6',
   'nissan': '1ea880a0-beb1-628c-ad31-02420a0002b6',
   'infiniti': '1ea880a0-beca-6a48-9341-02420a0002b6',
-  'lexus': '1ea880a0-bebf-6ac6-8a2f-02420a0002b6'
+  'lexus': '1ea880a0-bebf-6ac6-8a2f-02420a0002b6',
+  'kia': '1ea880a0-bebf-6864-a7a0-02420a0002b6',
+  'hyundai': '1ea880a0-bec5-6778-b790-02420a0002b6'
 }
