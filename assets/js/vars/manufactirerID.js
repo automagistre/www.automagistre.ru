2508b7c0533f19484012d7eba0fc93049e08e785
Kirill Sidorov <kirillsidorov@gmail.com>
2021-01-06 22:10:44 +0300

    fix ids kia and hyundai

--- assets/js/vars/manufactirerID.js
+++ assets/js/vars/manufactirerID.js
@@ -3,6 +3,6 @@ export const manufacturerID = {
   'nissan': '1ea880a0-beb1-628c-ad31-02420a0002b6',
   'infiniti': '1ea880a0-beca-6a48-9341-02420a0002b6',
   'lexus': '1ea880a0-bebf-6ac6-8a2f-02420a0002b6',
-  'kia': '1ea880a0-bebf-6864-a7a0-02420a0002b6',
-  'hyundai': '1ea880a0-bec5-6778-b790-02420a0002b6'
+  'hyundai': '1ea880a0-bebf-6864-a7a0-02420a0002b6',
+  'kia': '1ea880a0-bec5-6778-b790-02420a0002b6'
 }
