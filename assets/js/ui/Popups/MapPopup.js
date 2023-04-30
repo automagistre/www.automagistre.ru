2ad38e52deafa5c66ce278b61aac3a9cbf943efa
Kirill Sidorov <kirillsidorov@gmail.com>
2023-04-30 03:23:29 +0300

    Change map

--- assets/js/ui/Popups/MapPopup.js
+++ assets/js/ui/Popups/MapPopup.js
@@ -4,9 +4,9 @@ class MapPopup extends Popup {
   renderPopupBody() {
     return `<h4 class="popup__title">Схема проезда</h4>
             <div class="popup-map">
-              <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A27ae86f43acc5c257f2c380fe9b7d4e2d637fa87f8bbdd5c0770696dee3b3532&amp;source=constructor" 
-                      width="100%" 
-                      height="100%" 
+              <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Afb09d454a75025a7752b7b018d929d773c9837847eb4dc76fd098c02aad12c77&amp;source=constructor"
+                      width="100%"
+                      height="100%"
                       frameborder="0"></iframe>
             </div>`;
   }
